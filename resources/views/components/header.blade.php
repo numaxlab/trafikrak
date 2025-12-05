8909ff52f07fb3f4feb200cd07230a3e1f99ba80
Adrián P. Blunier <adrian@numax.org>
2025-12-05 12:02:06 +0100

    Engadido logo

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -6,8 +6,8 @@
 >
     <div class="container mx-auto px-4">
         <header class="org-site-header border-b-0 lg:gap-10">
-            <a class="text-xl font-bold" href="{{ route('trafikrak.storefront.homepage') }}" wire:navigate>
-                {{ config('app.name') }}
+            <a class="w-25 h-7.2 lg:w-40 lg:h-11" href="{{ route('trafikrak.storefront.homepage') }}" wire:navigate>
+                <x-trafikrak::logo/>
             </a>
 
             <div class="lg:hidden">

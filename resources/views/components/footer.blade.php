01683420bdfad54bc1b8c9e02c4588f2b6aed933
Adrián P. Blunier <adrian@numax.org>
2025-12-18 10:59:46 +0100

    added project sponsors logos to footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,26 @@
 <div class="bg-black text-white mt-20">
     <footer class="container mx-auto px-4 pt-10 pb-40">
-        Pié de página
+        <p class="at-small">
+            Este proyecto ha sido financiado por la Unión Europea.<br>
+            Next Generation, Plan de Recuperación, Ministerio de Trabajo y Economía Social.
+        </p>
+
+        <ul class="flex flex-wrap gap-6 mt-6">
+            <li>
+                <a href="https://next-generation-eu.europa.eu/" target="_blank" class="text-white block h-10 lg:h-14">
+                    @include('testa::logos.eu-next-generation')
+                </a>
+            </li>
+            <li>
+                <a href="https://www.mites.gob.es/" target="_blank" class="text-white block h-10 lg:h-14">
+                    @include('testa::logos.ministerio-tes')
+                </a>
+            </li>
+            <li>
+                <a href="https://www.mites.gob.es/" target="_blank" class="text-white block h-10 lg:h-14">
+                    @include('testa::logos.perte')
+                </a>
+            </li>
+        </ul>
     </footer>
 </div>
